ba37a77f10b6719ef49a10bcdd1ca7b8fc315f71
Denys <denisbadakaty51@gmail.com>
2023-09-17 02:11:44 +0300

    added error handling in city service

--- server/src/city/city.service.ts
+++ server/src/city/city.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
 import { City } from '@prisma/client';
 import { PrismaService } from '@prisma/prisma.service';
 
@@ -7,20 +7,28 @@ export class CityService {
   constructor(private readonly prisma: PrismaService) {}
 
   async findOne(cityId: number): Promise<City> {
-    const city = await this.prisma.city.findFirst({
-      where: { id: cityId },
-      include: {
-        weather: { include: { currentWeather: true, forecastWeather: true } },
-      },
-    });
-    return city;
+    try {
+      const city = await this.prisma.city.findFirst({
+        where: { id: cityId },
+        include: {
+          weather: { include: { currentWeather: true, forecastWeather: true } },
+        },
+      });
+      return city;
+    } catch (error) {
+      throw new ConflictException('Error getting a city');
+    }
   }
 
   async findMany(name: string): Promise<City[]> {
-    const exactCity = await this.prisma.city.findMany({ where: { name } });
-    const cities = await this.prisma.city.findMany({
-      where: { name: { startsWith: name, not: name } },
-    });
-    return [...exactCity, ...cities];
+    try {
+      const exactCity = await this.prisma.city.findMany({ where: { name } });
+      const cities = await this.prisma.city.findMany({
+        where: { name: { startsWith: name, not: name } },
+      });
+      return [...exactCity, ...cities];
+    } catch (error) {
+      throw new ConflictException('Error finding a cities');
+    }
   }
 }
