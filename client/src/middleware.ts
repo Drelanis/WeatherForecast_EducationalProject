5a78f25f6fbbb94fff1889767898d733973a94cb
Denys <denisbadakaty51@gmail.com>
2023-10-04 10:12:42 +0300

    REFACTORED middleware

--- client/src/middleware.ts
+++ client/src/middleware.ts
@@ -4,8 +4,10 @@ export const middleware = (request: NextRequest) => {
   if (!request.cookies.has('refreshtoken')) {
     return NextResponse.redirect(`${process.env.CLIENT_URL}/login`);
   }
+  NextResponse.redirect(request.url);
+  return NextResponse.next();
 };
 
 export const config = {
-  matcher: ['/profile'],
+  matcher: ['/profile', '/home', '/weather'],
 };
