392ea304bf6081055e879598e24767e7eefc58ff
Denys <denisbadakaty51@gmail.com>
2023-10-04 10:12:29 +0300

    REFACTORED navigation menu

--- client/src/app/components/Header/components/NavigationMenu/NavigationMenu.tsx
+++ client/src/app/components/Header/components/NavigationMenu/NavigationMenu.tsx
@@ -2,36 +2,21 @@
 import { FC } from 'react';
 import Button from '@mui/material/Button';
 import NavigationPopup from '@components/Header/components/NavigationPopup/NavigationPopup';
-import { useRouter } from 'next/navigation';
+import usePrivateUrl from '@hooks/usePrivateUrl';
 
 interface NavigationMenuProps {
   className: string;
 }
 
 const NavigationMenu: FC<NavigationMenuProps> = ({ className }) => {
-  const router = useRouter();
+  const [redirect] = usePrivateUrl();
+
   return (
     <div className={className}>
-      <Button
-        id="home-button"
-        onClick={() => {
-          if (!localStorage.getItem('accessToken')) {
-            return router.push('/login');
-          }
-          router.push('/home');
-        }}
-      >
+      <Button id="home-button" onClick={() => redirect('/home')}>
         Home
       </Button>
-      <Button
-        id="weather-button"
-        onClick={() => {
-          if (!localStorage.getItem('accessToken')) {
-            return router.push('/login');
-          }
-          router.push('/weather');
-        }}
-      >
+      <Button id="weather-button" onClick={() => redirect('/weather')}>
         Weather
       </Button>
       <NavigationPopup />
