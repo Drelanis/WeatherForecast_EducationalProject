0feb485335fa7f40f3e422d339e6c95be210a3d2
Denys <denisbadakaty51@gmail.com>
2023-10-13 17:10:00 +0300

    added animation for cards

--- client/src/app/weather/components/CityCard.tsx
+++ client/src/app/weather/components/CityCard.tsx
@@ -2,24 +2,31 @@ import React, { FC } from 'react';
 import { ICity } from '@lib/intarfaces';
 import Card from '../common/Card';
 import WeatherLoader from '../common/WeatherLoader';
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
+import AddCityCard from './AddCityCard';
 
-interface ICityCardProps {
+interface ICityCardsProps {
   cities: ICity[];
   loading: boolean;
+  setOpenModal: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
-const CityCard: FC<ICityCardProps> = ({ cities, loading }) => {
+const CityCards: FC<ICityCardsProps> = ({ cities, loading, setOpenModal }) => {
+  const handleOpenModal = () => setOpenModal(true);
   if (loading) {
     return <WeatherLoader loading={loading} />;
   }
 
   return (
-    <>
+    <TransitionGroup className="city-card-container">
+      <AddCityCard handleOpenModal={handleOpenModal} />
       {cities?.map((city: ICity) => (
-        <Card key={city.id} info={city} />
+        <CSSTransition key={city.id} timeout={500} classNames="card">
+          <Card key={city.id} info={city} />
+        </CSSTransition>
       ))}
-    </>
+    </TransitionGroup>
   );
 };
 
-export default CityCard;
+export default CityCards;
--- client/src/app/weather/page.tsx
+++ client/src/app/weather/page.tsx
@@ -1,19 +1,20 @@
 'use client';
 import React, { useState } from 'react';
-import AddCityCard from 'src/app/weather/components/AddCityCard';
-import CityCard from './components/CityCard';
+import CityCards from './components/CityCard';
 import useFindUsersCities from '@hooks/useFindUsersWeather';
 import ModalWindow from './common/ModalWindow';
 
 const Weather = () => {
   const { cities, loading } = useFindUsersCities();
   const [isOpenModal, setOpenModal] = useState(false);
-  const handleOpenModal = () => setOpenModal(true);
 
   return (
     <div className="weather-container">
-      <AddCityCard handleOpenModal={handleOpenModal} />
-      <CityCard cities={cities} loading={loading} />
+      <CityCards
+        cities={cities}
+        loading={loading}
+        setOpenModal={setOpenModal}
+      />
       <ModalWindow isOpenModal={isOpenModal} setOpenModal={setOpenModal} />
     </div>
   );
