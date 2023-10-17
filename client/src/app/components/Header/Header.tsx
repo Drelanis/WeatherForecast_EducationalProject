6f4090f450ba75f7a457f364f0d0357ba3847f96
Denys <denisbadakaty51@gmail.com>
2023-10-17 11:36:47 +0300

    ADDED background and some ui styles

--- client/src/app/components/Header/Header.tsx
+++ client/src/app/components/Header/Header.tsx
@@ -1,8 +1,7 @@
 'use client';
 import React from 'react';
 import NavigationMenu from './components/NavigationMenu/NavigationMenu';
-import styles from './header.module.css';
 
 export const Header = () => {
-  return <NavigationMenu className={styles.header} />;
+  return <NavigationMenu className="header" />;
 };
