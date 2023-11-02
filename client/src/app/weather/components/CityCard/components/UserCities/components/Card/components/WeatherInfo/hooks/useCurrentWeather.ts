df13ecab7c3bbcbbad07381893748b0041a6c133
Denys <denisbadakaty51@gmail.com>
2023-11-02 15:36:16 +0200

    ADDED handler errors on client

--- client/src/app/weather/components/CityCard/components/UserCities/components/Card/components/WeatherInfo/hooks/useCurrentWeather.ts
+++ client/src/app/weather/components/CityCard/components/UserCities/components/Card/components/WeatherInfo/hooks/useCurrentWeather.ts
@@ -2,9 +2,10 @@ import { useQuery, useSubscription } from '@apollo/client';
 import { GET_CURRENT_WEATHER } from '@apolloGraphQL/query/getCurrentWeather';
 import { UPDATED_CURRENT_WEATHER } from '@apolloGraphQL/subscriptions/updated-current-weather.subscription';
 import { ICity } from '@lib/intarfaces';
+import { toast } from 'react-toastify';
 
 const useCurrentWeather = (info: ICity) => {
-  const { data, loading } = useQuery(GET_CURRENT_WEATHER, {
+  const { data, loading, error } = useQuery(GET_CURRENT_WEATHER, {
     variables: { cityId: Number(info.id) },
   });
 
@@ -15,6 +16,10 @@ const useCurrentWeather = (info: ICity) => {
 
   const currentWeather =
     currentWeatherUpdated?.currentWeatherUpdated || data?.getCurrentWeather;
+
+  if (error) {
+    toast.error(error.message);
+  }
 
   return { currentWeather, loading };
 };
