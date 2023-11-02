52e21560ac3e2dbbb7fb1dba9b3648a084b6b186
Denys <denisbadakaty51@gmail.com>
2023-11-02 10:12:25 +0200

    FIXED graphql subscriptions

--- mobile/apollo/client.ts
+++ mobile/apollo/client.ts
@@ -1,9 +1,86 @@
-import { ApolloClient, InMemoryCache } from '@apollo/client';
+import {
+  ApolloClient,
+  ApolloLink,
+  createHttpLink,
+  FetchResult,
+  InMemoryCache,
+  Observable,
+  split,
+} from '@apollo/client';
+import { onError } from '@apollo/client/link/error';
+import { REFRESH } from './mutation/refreshTokens';
+import { GraphQLWsLink } from '@apollo/client/link/subscriptions';
+import { createClient } from 'graphql-ws';
+import { getMainDefinition } from '@apollo/client/utilities';
+
+const httpLink = createHttpLink({
+  uri: 'http://localhost:7000/graphql',
+  credentials: 'include',
+});
+
+const wsLink = new GraphQLWsLink(
+  createClient({
+    url: 'ws://localhost:7000/subscriptions',
+  })
+);
+
+const errorLink = onError(
+  ({ graphQLErrors, networkError, operation, forward }) => {
+    if (graphQLErrors) {
+      for (let err of graphQLErrors) {
+        switch (err.extensions?.code) {
+          case 'UNAUTHENTICATED':
+            if (operation.operationName === 'RefreshTokens') return;
+            const observable = new Observable<FetchResult<Record<string, any>>>(
+              (observer) => {
+                (async () => {
+                  try {
+                    await client.mutate({
+                      mutation: REFRESH,
+                    });
+                    const subscriber = {
+                      next: observer.next.bind(observer),
+                      error: observer.error.bind(observer),
+                      complete: observer.complete.bind(observer),
+                    };
+                    forward(operation).subscribe(subscriber);
+                  } catch (err) {
+                    observer.error(err);
+                  }
+                })();
+              }
+            );
+            return observable;
+        }
+      }
+    }
+    if (networkError) console.error(`[Network error]: ${networkError}`);
+  }
+);
+
+const splitLink = split(
+  ({ query }) => {
+    const definition = getMainDefinition(query);
+    return (
+      definition.kind === 'OperationDefinition' &&
+      definition.operation === 'subscription'
+    );
+  },
+  wsLink,
+  httpLink
+);
 
 const client = new ApolloClient({
-  uri: 'http://localhost:7000/graphql',
-  cache: new InMemoryCache(),
-  credentials: 'include',
+  link: ApolloLink.from([errorLink, splitLink]),
+  connectToDevTools: true,
+  cache: new InMemoryCache({
+    typePolicies: {
+      UnconventionalRootQuery: {
+        queryType: true,
+        mutationType: true,
+      },
+    },
+  }),
 });
 
 export default client;
